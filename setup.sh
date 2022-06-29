7bcc2835d7ab4d3df21cacc82bc27be7e7704de3
CheariX <Christian.Mainka@rub.de>
2022-06-29 19:48:34 +0200

    Copy keys directly from /etc/pki/akmods/{certs,private}

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 mkdir -p /root/rpmbuild/SOURCES/
-cp macros.kmodtool private_key.priv public_key.der /root/rpmbuild/SOURCES
+cp \
+  macros.kmodtool \
+  /etc/pki/akmods/private/private_key.priv \
+  /etc/pki/akmods/certs/public_key.der \
+  /root/rpmbuild/SOURCES
 rpmbuild -ba akmods-keys.spec
+rm -rf /root/rpmbuild/
 mv /root/rpmbuild/RPMS/noarch/akmods-keys-0.0.2-8.fc36.noarch.rpm .
