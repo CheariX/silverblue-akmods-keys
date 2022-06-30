d05649200e5c3eeba7b57b55870746cef9d9308f
Nelson Aloysio <26492660+nelsonaloysio@users.noreply.github.com>
2022-07-01 00:26:46 +0100

    Updated temporary files removal.

--- setup.sh
+++ setup.sh
@@ -7,5 +7,7 @@ cp \
   /etc/pki/akmods/certs/public_key.der \
   /root/rpmbuild/SOURCES
 rpmbuild -ba akmods-keys.spec
-rm -rf /root/rpmbuild/
 mv /root/rpmbuild/RPMS/noarch/akmods-keys-0.0.2-8.fc36.noarch.rpm .
+rm -df \
+  $(find /root/rpmbuild -type f | grep -E 'akmods-keys|public_key.der|private_key.priv|macros.kmodtool') \
+  $(find /root/rpmbuild -type d | sort -r)
